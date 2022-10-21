a46d378696d894c96535b55cd6386773d4a39de2
Fabricio <fabricioxgalarzamirada15@gmail.com>
2022-10-21 13:09:38 -0500

    ejemplo

---
+++ respuesta.php
@@ -0,0 +1,28 @@
+<!-- Autor: Fabricio Galarza Mirada-->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Registro Completo</title>
+	
+</head>
+
+<body>
+	<center><fieldset>
+		<h1>Completaste tu Registro</h1>
+		<p>Gracias por ingresar su información, ya se registraron sus datos. </p>
+		<a id="regresar" href="TrabajoFinal.html">  VOLVER A REGISTRARSE  </a>
+
+	</center></fieldset>
+
+	<?php
+	$nombre= $_GET["txtNombre"];
+	$nombre= $_GET["txtApellido"];
+	$nombre= $_GET["num"];
+	$nombre= $_GET["txtEmail"];
+?>
+
+	
+
+</body>
+</html>
